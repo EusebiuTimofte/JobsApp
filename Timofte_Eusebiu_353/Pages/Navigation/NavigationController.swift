30dc8cc3a099bdf85abef4fe7a662cebeb6a9842
user169475 <user169475@FF029.local>
2020-04-15 22:19:40 +0200

    title to navigation view items

---
+++ Timofte_Eusebiu_353/Pages/Navigation/NavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationController.swift
+//  Timofte_Eusebiu_353
+//
+//  Created by user169475 on 4/15/20.
+//  Copyright © 2020 user169246. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
