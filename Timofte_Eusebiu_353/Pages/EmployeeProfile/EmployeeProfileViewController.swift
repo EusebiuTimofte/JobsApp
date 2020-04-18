1c79a3da7c1454c11e9cc75c86373ad3d89f2f04
user169480 <user169480@FF033.local>
2020-04-18 12:52:40 +0200

    started creating keyword labels programmatically

---
+++ Timofte_Eusebiu_353/Pages/EmployeeProfile/EmployeeProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EmployeeProfileViewController.swift
+//  Timofte_Eusebiu_353
+//
+//  Created by user169480 on 4/18/20.
+//  Copyright © 2020 user169246. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeProfileViewController: UIViewController {
+
+    let userKeywords: [String] = ["it", "pharma"]
+    @IBOutlet var rootView: UIView!
+    @IBOutlet weak var keywords: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let label0 = UILabel()
+        label0.text = userKeywords[0]
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
