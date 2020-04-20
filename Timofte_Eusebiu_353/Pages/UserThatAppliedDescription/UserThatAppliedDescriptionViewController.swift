ae21f60ba39c5cc9b0c1befc02740d0f7088476a
user169480 <user169480@FF033.local>
2020-04-20 19:31:45 +0200

    created class for user that applied description

---
+++ Timofte_Eusebiu_353/Pages/UserThatAppliedDescription/UserThatAppliedDescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserThatAppliedDescriptionViewController.swift
+//  Timofte_Eusebiu_353
+//
+//  Created by user169480 on 4/20/20.
+//  Copyright © 2020 user169246. All rights reserved.
+//
+
+import UIKit
+
+class UserThatAppliedDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
